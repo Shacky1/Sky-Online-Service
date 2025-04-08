fbdfdbe771bab752569109693cd7fba49b2968ef
Shacky1 <meshackvenance7@gamil.com>
2025-04-09 00:18:25 +0300

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/shacky/materialmanagement/service/AdminService.java
+++ src/main/java/com/shacky/materialmanagement/service/AdminService.java
@@ -3,9 +3,10 @@ package com.shacky.materialmanagement.service;
 import com.shacky.materialmanagement.entity.Admin;
 import com.shacky.materialmanagement.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
+@Service
 public class AdminService {
     @Autowired
     private AdminRepository adminRepository;
